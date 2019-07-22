50f6501f97bb71704be0f65b49d5ec6384601b81
rizeup88 <marinersrock88@yahoo.com>
2019-07-22 14:16:17 -0700

    This commit will present a complete list of terms and definitions related to what has been learned in JavaScript and jQuery.

---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+  $(".clickable-js").click(function() {
+    $(".js-showing").toggle();
+    $(".js-hidden").toggle();
+  });
+
+  $(".clickable-op").click(function() {	
+  	$(".op-showing").toggle();
+    $(".op-hidden").toggle();
+  });
+
+  $(".clickable-vb").click(function() {
+  	$(".vb-showing").toggle();
+    $(".vb-hidden").toggle();
+  });
+
+  $(".clickable-vn").click(function() {
+  	$(".vn-showing").toggle();
+    $(".vn-hidden").toggle();
+  });
+
+  $(".clickable-fn").click(function() {
+  	$(".fn-showing").toggle();
+    $(".fn-hidden").toggle();
+  });
+
+  $(".clickable-md").click(function() {
+  	$(".md-showing").toggle();
+    $(".md-hidden").toggle();
+  });
+
+  $(".clickable-ar").click(function() {
+  	$(".ar-showing").toggle();
+    $(".ar-hidden").toggle();
+  });
+
+  $(".clickable-pm").click(function() {
+  	$(".pm-showing").toggle();
+    $(".pm-hidden").toggle();
+  });
+
+  $(".clickable-rt").click(function() {
+  	$(".rt-showing").toggle();
+    $(".rt-hidden").toggle();
+  });
+
+  $(".clickable-cm").click(function() {
+  	$(".cm-showing").toggle();
+    $(".cm-hidden").toggle();
+  });
+
+  $(".clickable-st").click(function() {
+  	$(".st-showing").toggle();
+    $(".st-hidden").toggle();
+  });
+
+  $(".clickable-bo").click(function() {
+  	$(".bo-showing").toggle();
+    $(".bo-hidden").toggle();
+  });
+
+  $(".clickable-df").click(function() {
+  	$(".df-showing").toggle();
+    $(".df-hidden").toggle();
+  });
+
+  $(".clickable-mt").click(function() {
+  	$(".mt-showing").toggle();
+    $(".mt-hidden").toggle();
+  });
+
+  $(".clickable-nn").click(function() {
+  	$(".nn-showing").toggle();
+    $(".nn-hidden").toggle();
+  });
+
+  $(".clickable-es").click(function() {
+  	$(".es-showing").toggle();
+    $(".es-hidden").toggle();
+  });
+
+  $(".clickable-al").click(function() {
+  	$(".al-showing").toggle();
+    $(".al-hidden").toggle();
+  });
+
+  $(".clickable-co").click(function() {
+  	$(".co-showing").toggle();
+    $(".co-hidden").toggle();
+  });
+
+  $(".clickable-jq").click(function() {
+  	$(".jq-showing").toggle();
+    $(".jq-hidden").toggle();
+  });
+
+  $(".clickable-ab").click(function() {
+  	$(".ab-showing").toggle();
+    $(".ab-hidden").toggle();
+  });
+});
